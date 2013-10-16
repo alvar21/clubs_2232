f5d23a6fa11f16c59a012dedd6b9b40c136bde6e
Alvar <alvar.ongkowidjaja@gmail.com>
2013-10-16 21:48:38 +0800

    Updated settings

--- project/settings.py
+++ project/settings.py
@@ -24,8 +24,8 @@ DATABASES = {
         'NAME': 'clubs',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'root',
-        'PASSWORD': 'FollowChrist',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'PASSWORD': '',
+        # 'HOST': 'ec2-54-252-182-144.ap-southeast-2.compute.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
