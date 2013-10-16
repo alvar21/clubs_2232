656140b75c17f9ed9f87ad25bfdc9707c47b3c5c
starlest <Edwin@Edwins-MacBook-Pro.local>
2013-10-16 14:59:23 +0800

    small tweaks

--- project/settings.py
+++ project/settings.py
@@ -17,15 +17,14 @@ MANAGERS = ADMINS
 
 if os.getcwd() == "/app":
     DATABASES = { 'default': dj_database_url.config(default='postgres://localhost') }
-# default="mysql://root:FollowChrist@localhost:3306/clubs"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'clubs',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
+        'USER': '',
         'PASSWORD': '',
-        # 'HOST': 'ec2-54-252-182-144.ap-southeast-2.compute.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'HOST': 'ec2-54-252-182-144.ap-southeast-2.compute.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
