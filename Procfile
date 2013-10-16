7a8fdb373ba7b3d66d244a97a97f27890ce85011
Alvar <alvar.ongkowidjaja@gmail.com>
2013-10-17 01:53:04 +0800

    Final production stage

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 # web: gunicorn clubs.wsgi
-web: python clubs_2232/manage.py runserver 0.0.0.0:$PORT --noreload
+web: python manage.py runserver 0.0.0.0:$PORT --noreload
--- project/settings.py
+++ project/settings.py
@@ -15,22 +15,10 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+# PRODUCTION
 if os.getcwd() == "/app":
     DATABASES = { 'default': dj_database_url.config(default='postgres://localhost') }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'clubs',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': 'ec2-54-252-182-144.ap-southeast-2.compute.amazonaws.com',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
-        # 'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        # 'PORT': '3306',                      # Set to empty string for default.
-    }
-}
-    
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
